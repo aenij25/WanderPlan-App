437e6ac012e77b415c9953007982a8e64c6e6275
lifahjdm <lifahjayadimerta@gmail.com>
2023-12-13 20:45:34 +0700

    layout

--- app/src/main/java/com/aplikasi/wanderplan/Activity/HomeDashboard.java
+++ app/src/main/java/com/aplikasi/wanderplan/Activity/HomeDashboard.java
@@ -1,5 +1,7 @@
 package com.aplikasi.wanderplan.Activity;
 
+import static com.aplikasi.wanderplan.R.id.top_places_recycler;
+
 import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -29,19 +31,26 @@ public class HomeDashboard extends AppCompatActivity {
             List<WisataData> wisataDataList = new ArrayList<>();
             wisataDataList.add(new WisataData(R.drawable.bali_homescreen));
             wisataDataList.add(new WisataData(R.drawable.mountain));
-            wisataDataList.add(new WisataData(R.drawable.bali_homescreen));
-            wisataDataList.add(new WisataData(R.drawable.mountain));
-            wisataDataList.add(new WisataData(R.drawable.bali_homescreen));
-            wisataDataList.add(new WisataData(R.drawable.mountain));
+            wisataDataList.add(new WisataData(R.drawable.imgwisata_komodo_island_1));
+            wisataDataList.add(new WisataData(R.drawable.imgwisata_raja_ampat_1));
+            wisataDataList.add(new WisataData(R.drawable.imgwisata_gili_trawangan_1));
+            wisataDataList.add(new WisataData(R.drawable.imgwisata_lombok_mandalika_1));
+            wisataDataList.add(new WisataData(R.drawable.imgwisata_bukit_marinding_waingapu_1));
+            wisataDataList.add(new WisataData(R.drawable.imgwisata_bunaken_1));
+            wisataDataList.add(new WisataData(R.drawable.imgwisata_taman_nasional_bromo_semeru_1));
+            wisataDataList.add(new WisataData(R.drawable.imgwisata_danau_toba_1));
+
 
             setWisataRecycler(wisataDataList);
 
             List<HotelData> hotelDataList = new ArrayList<>();
             hotelDataList.add(new HotelData(R.drawable.detailhotel1));
-            hotelDataList.add(new HotelData(R.drawable.detailhotel1));
-            hotelDataList.add(new HotelData(R.drawable.detailhotel1));
-            hotelDataList.add(new HotelData(R.drawable.detailhotel1));
-            hotelDataList.add(new HotelData(R.drawable.detailhotel1));
+            hotelDataList.add(new HotelData(R.drawable.imghotel_adiwana_bisma_1));
+            hotelDataList.add(new HotelData(R.drawable.imghotel_g_h_universal_1));
+            hotelDataList.add(new HotelData(R.drawable.imghotel_hotel_tentrem_semarang_1));
+            hotelDataList.add(new HotelData(R.drawable.imghotel_padma_resort_ubud_1));
+            hotelDataList.add(new HotelData(R.drawable.imghotel_the_kayon_jungle_resort_1));
+            hotelDataList.add(new HotelData(R.drawable.imghotel_the_seminyak_beach_resort_1));
 
             setHotelRecycler(hotelDataList);
 
@@ -57,7 +66,7 @@ public class HomeDashboard extends AppCompatActivity {
         }
 
         private void setHotelRecycler(List<HotelData> hotelDataList) {
-            HotelRecycler = findViewById(R.id.top_places_recycler);
+            HotelRecycler = findViewById(top_places_recycler);
             RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(this, RecyclerView.HORIZONTAL, false);
             HotelRecycler.setLayoutManager(layoutManager);
             hotelAdapter = new HotelAdapter(this, hotelDataList);
